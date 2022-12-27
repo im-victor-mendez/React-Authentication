ea3ea191f4fb95090810fc786f598e7ba181c95b
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-27 12:25:34 -0600

    Defined firebase connection.

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,24 @@
-import dotenv from "dotenv";
-
-dotenv.config()
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = process.env.FIREBASE_CONFIG
+
+//To Find a way to use with .env file
+const firebaseConfig = 
+  {
+    apiKey: "AIzaSyBooH2lrYw7IKt1YtELePbmdcItmqGjKt0",
+    authDomain: "react-login-47279.firebaseapp.com",
+    projectId: "react-login-47279",
+    storageBucket: "react-login-47279.appspot.com",
+    messagingSenderId: "577416143260",
+    appId: "1:577416143260:web:f2e01878ecc583bb98a859",
+    measurementId: "G-4BKP3MV4W5"
+  }
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
